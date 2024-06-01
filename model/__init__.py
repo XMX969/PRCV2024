23f48e1763c2f04b22bb4b54cb058789830ccd8d
XMX969 <60999216+XMX969@users.noreply.github.com>
2024-06-01 17:16:58 +0800

    Create __init__.py

---
+++ model/__init__.py
@@ -0,0 +1,13 @@
+from model.Unet.model_Unet import U_Net as Unet
+from model.ACM.model_ALCnet import ASKCResNetFPN as ALCNet
+from model.ACM.model_ACM import ASKCResUNet as ACM
+from model.DNANet.model_DNANet import DNANet as DNANet
+# from model.ISNet.model_ISNet import ISNet as ISNet
+from model.RISTDnet.model_RISTDnet import RISTDnet as RISTDnet
+from model.UIUNet.model_UIUNet import UIUNET as UIUNet
+from model.ISTDUNet.model_ISTDUNet import ISTDU_Net as ISTDU_Net
+from model.RDIAN.model_RDIAN import RDIAN as RDIAN
+from model.ABCNet.model_ABCNet import ABCNet as ABCNet
+from model.MTUNet.model_MTUNet import MTUNet as MTUNet
+from model.URANet.model_URANet import URANet as URANet
+from model.SeRankDet.model_SeRankDet import SeRankDet as SeRankDet
