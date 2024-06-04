f6475d9e03b98a05553b2a171f30ce6f9ae7ac10
XMX969 <60999216+XMX969@users.noreply.github.com>
2024-06-04 15:50:46 +0800

    Update count.py

--- count.py
+++ count.py
@@ -3,8 +3,7 @@ def count_lines_in_txt(txt_file):
         lines = file.readlines()
         return len(lines)
 
-# 示例用法
-txt_file = 'datasets/WideIRSTD/img_idx/test_WideIRSTD.txt'  # 替换成你的txt文件路径
+txt_file = 'datasets/WideIRSTD/img_idx/test_WideIRSTD.txt'
 line_count = count_lines_in_txt(txt_file)
 print(f'The number of lines in the txt file is: {line_count}')
 
@@ -12,11 +11,12 @@ import os
 
 def count_files_in_folder(folder_path):
     file_count = 0
-    for _, _, files in os.walk(folder_path):
-        file_count += len(files)
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.png'):
+            file_count += 1
     return file_count
 
-# 示例用法
-folder_path = 'results/WideIRSTD/mask'  # 替换成你的文件夹路径
+# folder_path = 'results/WideIRSTD/ISTDU-Net'
+folder_path = 'results/WideIRSTD/mask'
 file_count = count_files_in_folder(folder_path)
 print(f'The number of files in the folder is: {file_count}')
